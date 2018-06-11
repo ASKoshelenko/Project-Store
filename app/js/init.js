f5ff3865356fc74fee2fc79135ced3981ee318cf
ASKoshelenko <18021992alex@gmail.com>
2018-06-11 11:39:29 +0300

    Create Newest products, Week tranding carousel

---
+++ app/js/init.js
@@ -0,0 +1,10 @@
+$( function() {
+    //init week trending carousel
+    $('.week-trending-carousel').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000      
+    });
+
+} );
