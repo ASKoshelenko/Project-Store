4bddc1ae3a52852c698184874c0d8aeb31af2e1e
ASKoshelenko <18021992alex@gmail.com>
2018-06-13 11:32:52 +0300

    Add lesson 15 code, rework price range, create products-details.html

--- app/js/init.js
+++ app/js/init.js
@@ -1,10 +1,25 @@
 $( function() {
     //init week trending carousel
     $('.week-trending-carousel').slick({
-        arrows: false,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 5000      
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 5000      
     });
 
 } );
+
+( function($) {    
+    //init ion range slider
+    $("#price-range").ionRangeSlider({
+        type: "double",
+    grid: false,
+    min: 0,
+    max: 1000,
+    from: 200,
+    to: 800,
+    prefix: "$",
+    hide_min_max: true
+    });
+    
+})(jQuery);
